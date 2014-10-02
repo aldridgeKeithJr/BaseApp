c9271706ffbbb141fbbc83946735c3562d958f62
aldridgeKeithJr <aldridge.keith.jr@gmail.com>
2014-10-02 14:22:14 -0400

    Base app
    Application template to apply security

---
+++ BaseApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaseApp.Startup))]
+namespace BaseApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
